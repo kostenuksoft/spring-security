c9dc62dcd3111ac04afaa85a0b3714896215f13d
Repetovskyi V. <kostenuksoft@outlook.jp>
2026-09-12 18:12:18 +0300

    add spring boot web skeleton

---
+++ src/main/java/com/repetovskyi/websecurity/WebSecurityApplication.java
@@ -0,0 +1,11 @@
+package com.repetovskyi.websecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecurityApplication {
+     static void main(String[] args) {
+        SpringApplication.run(WebSecurityApplication.class, args);
+    }
+}
